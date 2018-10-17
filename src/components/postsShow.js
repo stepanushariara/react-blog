ef126c91f56c0d77dfedc91841cfc7189ca45d83
Stepanus Hariara <stepanus.hariara@momobil.id>
2018-10-17 12:01:21 +0700

    section 102 + button delete functional working

--- src/components/postsShow.js
+++ src/components/postsShow.js
@@ -20,7 +20,7 @@ class PostsShow extends Component{
         return(
             <div>
                 <Link to="/">Back to index</Link>
-                <button className="btn btn-danger float-xs-right">Delete</button>
+                <button className="btn btn-danger float-md-right" onClick={this.onDelete.bind(this)}>Delete</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
